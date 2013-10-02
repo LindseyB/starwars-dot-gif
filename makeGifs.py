f024eeb3c2b593e216aff028e97477d734c73a52
Lindsey Bieda <hazardous.narayan@gmail.com>
2013-10-01 23:00:53 -0400

    I misread VLCs docs - it can use milliseconds in the start.
    I assumed when I read the manpage for VLC that start-time and end-time only took
    whole seconds, but rereading it actually takes a float so I can use milliseconds.
    This should ensure the gifs capture exactly what they should as long as the SRT
    files are properly synced.

--- makeGifs.py
+++ makeGifs.py
@@ -51,8 +51,10 @@ def makeGif(source, sub_index, rand=False):
 	if rand:
 		sub_index = random.randint(0, len(subs)-1)
 
-	start = (3600 * subs[sub_index].start.hours) + (60 * subs[sub_index].start.minutes) + subs[sub_index].start.seconds - 1
-	end = (3600 * subs[sub_index].end.hours) + (60 * subs[sub_index].end.minutes) + subs[sub_index].end.seconds + 1
+	print subs[sub_index].start.milliseconds
+
+	start = (3600 * subs[sub_index].start.hours) + (60 * subs[sub_index].start.minutes) + subs[sub_index].start.seconds + (0.001*subs[sub_index].start.milliseconds)
+	end = (3600 * subs[sub_index].end.hours) + (60 * subs[sub_index].end.minutes) + subs[sub_index].end.seconds + (0.001*subs[sub_index].end.milliseconds)
 	text = striptags(subs[sub_index].text).split("\n")
 
 	# tell vlc to go get images for gifs
