301b25e54df69f0607a53e7a9c3f6e0a7dd8e6f6
Lindsey Bieda <hazardous.narayan@gmail.com>
2013-10-28 09:30:14 -0400

    Tumblr bot!
    Closes #8

---
+++ tumblr_bot.py
@@ -0,0 +1,39 @@
+import random
+import os
+import ConfigParser
+import time
+
+from tumblpy import Tumblpy
+from makeGifs import makeGif
+
+config = ConfigParser.ConfigParser()
+config.read("config.cfg")
+config.sections()
+
+CONSUMER_KEY = config.get("tumblr", "consumer_key")
+CONSUMER_SECRET = config.get("tumblr", "consumer_secret")
+OAUTH_TOKEN = config.get("tumblr", "oauth_token")
+OAUTH_TOKEN_SECRET = config.get("tumblr", "oauth_token_secret")
+
+
+t = Tumblpy(
+    CONSUMER_KEY,
+    CONSUMER_SECRET,
+    OAUTH_TOKEN,
+    OAUTH_TOKEN_SECRET,
+)
+
+while True:
+	quote = makeGif(random.randint(4,6), 0, rand=True)
+	quote = ' '.join(quote)
+
+	while(os.path.getsize('star_wars.gif') > 1048576):
+		os.popen('convert star_wars.gif -resize 90% star_wars.gif')
+
+	photo = open('star_wars.gif', 'rb')
+
+	post = t.post('post', blog_url='http://starwarsgifsasaservice.tumblr.com', params={'type':'photo', 'caption': quote, 'data': photo})
+
+	print "sleeping..."
+	# sleep 12 hours
+	time.sleep(43200)
