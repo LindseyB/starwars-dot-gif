e85539718165255a4676417fe0ce6a2010f76df0
Josh <jromer94@gmail.com>
2015-01-29 17:54:35 -0500

    added exit button to search

--- star_wars_gif.py
+++ star_wars_gif.py
@@ -37,8 +37,11 @@ def search(button):
 	edit = urwid.Edit("Search quotes: ")
 	done = urwid.Button(u'Search')
 	urwid.connect_signal(done, 'click', find_quotes, edit)
+        exit_button = urwid.Button(u'Exit')
+        urwid.connect_signal(exit_button, 'click', exit)
 	main.original_widget = urwid.Filler(urwid.Pile([edit,
-		urwid.AttrMap(done, None, focus_map='reversed')]))
+		urwid.AttrMap(done, None, focus_map='reversed'),
+                urwid.AttrMap(exit_button, None, focus_map='reversed')]))
 
 def find_quotes(button, edit):
 	subs = pysrt.open(sub_files[source])
@@ -90,10 +93,15 @@ def generate_gif_with_subtitle(button, edit):
 	subtitle = edit.edit_text
 	raise urwid.ExitMainLoop()
 
+def exit(button):
+    index = None
+    raise urwid.ExitMainLoop()
+
 main = urwid.Padding(menu(u'Movie choice', choices), left=2, right=2)
 top = urwid.Overlay(main, urwid.SolidFill(u'\N{MEDIUM SHADE}'),
 	align='center', width=('relative', 60),
 	valign='middle', height=('relative', 60),
 	min_width=20, min_height=9)
 urwid.MainLoop(top, palette=[('reversed', 'standout', '')]).run()
-makeGif(source, index, custom_subtitle=subtitle)
+if index:
+    makeGif(source, index, custom_subtitle=subtitle)
